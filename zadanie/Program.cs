73df6dacbe4b33a99317366ac5a0975ee4f4f1ba
Jakubbbb12 <jakub.majcher@student.wsb.edu.pl>
2026-03-25 13:32:42 +0100

    Prototyp 1

---
+++ zadanie/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+//System playlist muzycznych – klasy Track, Playlist, User; tworzenie, modyfikacja i odtwarzanie list utworów.
+class User
+{
+    public String Username{ get; set;}
+
+
+}
+
+class Playlist
+{
+    public String PlaylistName{ get; set;}
+}
+
+class Track
+{
+    public String Singer{ get; set;}
+    public String Title{ get; set;}
+    public String Duration{ get; set;}
+    public String Genre{ get; set;}
+    public String AddedAt{ get; set;}
+    
+}
+
+
+
+
+
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
