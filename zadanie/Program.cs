91c03bd5694425cbc9929ae504d59154fa0b3616
Jakubbbb12 <jakub.majcher@student.wsb.edu.pl>
2026-04-01 13:12:32 +0200

    klasa Playlista
    Dodanie nowej klasy + właściwości + metod (add, remove)

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Reflection;
+using System.Collections.Generic;
+using System.Numerics;
 //System playlist muzycznych – klasy Track, Playlist, User; tworzenie, modyfikacja i odtwarzanie list utworów
 class Track
 {
-    public String Title{ get; set;}
-    public String Singer{ get; set;}
-    public int Duration{ get; set;}
-    public String Genre{ get; set;}
-    public DateTime AddedAt{ get; set;}
+    public String Title{ get; init;}
+    public String Singer{ get; init;}
+    public int Duration{ get; init;}
+    public String Genre{ get; init;}
+    public DateTime AddedAt{ get; init;}
 
     public Track(string title, string singer, int duration, string genre, DateTime addedAt)
     {
@@ -22,15 +23,61 @@ class Track
     {
         Console.WriteLine($"Tytuł: {Title}, Wykonawca: {Singer}");
     }
-    
 }
+//dodac czas lacznego czasu ze wszystkich trackow
+class Playlist
+{
+    public List<Track> Tracks{get; set;}
+    public int GetTrackCount()
+    {
+        return Tracks.Count;
+    }
 
+    public int GetTotalDuration()
+    {
+        int total = 0;
 
+        foreach (Track track in Tracks)
+        {
+            total = total + track.Duration;
+        }
 
+        return total;
+    }
 
+    public String PlaylistTitle{ get; set;}
 
+    public Playlist(string playlistTitle)
+    {
+        PlaylistTitle = playlistTitle;
+        Tracks = new List<Track>();
+    }
+    
 
+    public void PlaylistInfo()
+    {
+        Console.WriteLine($"Playlista {PlaylistTitle}, Ilość utworów: {GetTrackCount()}");
+        Console.WriteLine($"Całkowity czas playlisty: {GetTotalDuration()}");
 
+        foreach (Track track in Tracks)
+        {
+            track.TrackInfo();
+        }
+
+    }
+
+    public void AddTrack(Track track)
+    {
+        Tracks.Add(track);
+    }
+
+    public void RemoveTrack(string title)
+    {
+        Track toRemove = Tracks.Find(t => t.Title == title);
+        Tracks.Remove(toRemove);
+    }
+
+}
 
 
 class Program
